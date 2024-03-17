96283591645b9a733803600ee0a41c637ce77d7d
amit_picode <ajangir1511@gmail.com>
2024-03-17 10:11:31 +0530

    push

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
 
     // Note: There should not be trailing / and the end
     // API_URL: 'https://hisab-kitab-api-v2.onrender.com',
-    API_URL: 'http://localhost:5003',
+    API_URL: 'http://http://91.108.105.104:5003',
 
 };
 export default config;
