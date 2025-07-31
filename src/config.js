f8b36217699f3ebe3e4fe8ad0061582091d33f76
Vashesh08 <vashesh2001@gmail.com>
2025-07-31 21:07:13 +0530

    Credential Testing SUccesful

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ const config = {
 
     // Note: There should not be trailing / and the end
     // API_URL: 'https://hisab-kitab-api-v2.onrender.com',
-    API_URL: 'https://api.mehtajewellers.in',
-    // API_URL: 'http://localhost:5003'
+    // API_URL: 'https://api.mehtajewellers.in',
+    API_URL: 'http://localhost:5003'
 
 };
 export default config;
